f77ad55b76087d646a558f129f0ecfbe5a42f810
gchoi17 <gchoi17@illinois.edu>
2020-10-08 12:12:43 -0500

    fixed

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,28 @@
 // For future reference about Gulp 4 task execution: https://fettblog.eu/gulp-4-parallel-and-series/
  
 var gulp = require("gulp");
-var browserSync = require("browser-sync").create();
 var sass = require("gulp-sass");
- 
+var browserSync = require("browser-sync").create();
+
 // Compile sass into CSS & auto-inject into browsers
-gulp.task("sass", function()
-{
-  return gulp.src(["src/scss/**/*.scss"])
-  .pipe(sass())
-  .pipe(gulp.dest("src/css"))
+function style() {
+  return gulp.src('src/scss/**/*.scss')
+  .pipe(sass().on('error',sass.logError))
+  .pipe(gulp.dest('src/css'))
   .pipe(browserSync.stream());
-});
-exports.style=style;
-// Move the javascript files into our /src/js folder
-gulp.task("js", function()
-{
-    return gulp.src(["node_modules/bootstrap/dist/js/bootstrap.min.js", "node_modules/jquery/dist/jquery.min.js", "node_modules/popper.js/dist/umd/popper.min.js"])
-      .pipe(gulp.dest("src/js"))
-      .pipe(browserSync.stream());
-});
- 
-// Static server + watching for changes in scss/html files (recompiling scss files too)
-gulp.task("serve", function()
-{
+}
+
+function watch() {
   browserSync.init({
-    server:"./src"
+      server: {
+         baseDir: "./src",
+         index: "/index.html"
+      }
   });
- 
-  gulp.watch(["node_modules/bootstrap/scss/bootstrap.scss", "src/scss/*.scss"]).on("change", gulp.task("sass"));
-  gulp.watch("src/*.html").on("change", browserSync.reload);
-});
- 
-gulp.task("default", gulp.parallel(["js", "serve"]));
+  gulp.watch('src/scss/**/*.scss', style)
+  gulp.watch('./*.html').on('change',browserSync.reload);
+  gulp.watch('./js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.style = style;
+exports.watch = watch;
